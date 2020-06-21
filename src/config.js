e76e8177e9004cefa4591a94400a140602b24127
Ioannis Charalampidis <icharalampidis@d2iq.com>
2020-06-21 22:06:53 +0200

    Fixing typo on package.json config name

--- src/config.js
+++ src/config.js
@@ -1,20 +1,21 @@
-'use strict'
+"use strict";
 
-const findUp = require('findup-sync')
-const path = require('path')
-const merge = require('deepmerge')
+const findUp = require("findup-sync");
+const path = require("path");
+const merge = require("deepmerge");
 
-const { expandParametricConfig } = require('./env')
-const { ProjectConfig } = require('./struct/config')
-const { defaultContextForProject } = require('./context')
+const { expandParametricConfig } = require("./env");
+const { ProjectConfig } = require("./struct/config");
+const { defaultContextForProject } = require("./context");
 
-const PATH_PKGCONF = 'package.json'
-const PATH_USER = '.eilos.js'
-const PATH_BUILD = 'build'
-const PATH_NODE_MODULES = 'node_modules'
-const PRESET_PREFIX = 'eilos-preset-'
+const PATH_PKGCONF = "package.json";
+const PATH_USER = ".eilos.js";
+const PATH_BUILD = "build";
+const PATH_NODE_MODULES = "node_modules";
+const PRESET_PREFIX = "eilos-preset-";
 
-const overwriteMerge = (destinationArray, sourceArray, options) => [].concat(destinationArray, sourceArray)
+const overwriteMerge = (destinationArray, sourceArray, options) =>
+  [].concat(destinationArray, sourceArray);
 
 /**
  * Gets the full path to the elios node module
@@ -22,8 +23,8 @@ const overwriteMerge = (destinationArray, sourceArray, options) => [].concat(des
  * @returns {string}
  */
 exports.getEliosModulePath = () => {
-  return path.dirname(__filename)
-}
+  return path.dirname(__filename);
+};
 
 /**
  * Gets the top level path of the project aegir is executed in.
@@ -31,8 +32,8 @@ exports.getEliosModulePath = () => {
  * @returns {string}
  */
 exports.getBasePath = () => {
-  return process.cwd()
-}
+  return process.cwd();
+};
 
 /**
  * Gets the path to user's package.json file
@@ -40,8 +41,8 @@ exports.getBasePath = () => {
  * @returns {string}
  */
 exports.getPackageConfigPath = () => {
-  return path.join(exports.getBasePath(), PATH_PKGCONF)
-}
+  return path.join(exports.getBasePath(), PATH_PKGCONF);
+};
 
 /**
  * Returns the package configuration contents
@@ -49,8 +50,8 @@ exports.getPackageConfigPath = () => {
  * @returns {Object}
  */
 exports.getPackageConfig = () => {
-  return require(exports.getPackageConfigPath())
-}
+  return require(exports.getPackageConfigPath());
+};
 
 /**
  * Gets the `node_modules` directory position
@@ -58,8 +59,8 @@ exports.getPackageConfig = () => {
  * @returns {string}
  */
 exports.getModulesPath = () => {
-  return path.join(exports.getBasePath(), PATH_NODE_MODULES)
-}
+  return path.join(exports.getBasePath(), PATH_NODE_MODULES);
+};
 
 /**
  * Gets the build directory location
@@ -67,8 +68,8 @@ exports.getModulesPath = () => {
  * @returns {string}
  */
 exports.getBuildPath = () => {
-  return path.join(exports.getBasePath(), PATH_BUILD)
-}
+  return path.join(exports.getBasePath(), PATH_BUILD);
+};
 
 /**
  * Finds the engine configuration file in the project's directory
@@ -76,8 +77,8 @@ exports.getBuildPath = () => {
  * @returns {Object}
  */
 exports.getUserConfigPath = function () {
-  return findUp(PATH_USER)
-}
+  return findUp(PATH_USER);
+};
 
 /**
  * Returns the engine configuration
@@ -85,32 +86,32 @@ exports.getUserConfigPath = function () {
  * @returns {Object}
  */
 exports.getUserConfig = () => {
-  let conf = {}
+  let conf = {};
   try {
-    const path = exports.getUserConfigPath()
+    const path = exports.getUserConfigPath();
     if (!path) {
-      return {}
+      return {};
     }
-    conf = require(path)
+    conf = require(path);
   } catch (err) {
-    console.error(err) // eslint-disable-line no-console
+    console.error(err); // eslint-disable-line no-console
   }
-  return conf
-}
+  return conf;
+};
 
 /**
  * Load the package configuration
  */
 exports.getDepPackagePath = (name) => {
-  return path.join(exports.getModulesPath(), name)
-}
+  return path.join(exports.getModulesPath(), name);
+};
 
 /**
  * Load the package configuration
  */
 exports.getDepPackageConfig = (name) => {
-  return require(path.join(exports.getDepPackagePath(name), PATH_PKGCONF))
-}
+  return require(path.join(exports.getDepPackagePath(name), PATH_PKGCONF));
+};
 
 /**
  * Figure out the name of the preset to use
@@ -119,41 +120,41 @@ exports.getDepPackageConfig = (name) => {
  */
 exports.getPresetName = (engineConf, packageConf) => {
   // First prefer user-specified configuration
-  if (engineConf.preset) return engineConf.preset
+  if (engineConf.preset) return engineConf.preset;
 
   // Then guess the preset by scanning the package configuration names
   const allDeps = Object.assign(
     {},
     packageConf.dependencies,
     packageConf.devDependencies
-  )
+  );
   const pkgPresets = Object.keys(allDeps).filter((k) =>
     k.startsWith(PRESET_PREFIX)
-  )
+  );
 
   if (pkgPresets.length === 1) {
-    return pkgPresets[0]
+    return pkgPresets[0];
   } else if (pkgPresets.length > 1) {
     throw new Error(
       `Found more than 1 preset on your 'devDependencies', please specify which one to use in your ${PATH_USER} file`
-    )
+    );
   }
 
-  throw new Error('Could not find a preset to use')
-}
+  throw new Error("Could not find a preset to use");
+};
 
 /**
  * Load the preset configuration from the given
  */
 exports.getPresetConfig = (presetName) => {
-  const presetPkg = exports.getDepPackageConfig(presetName)
+  const presetPkg = exports.getDepPackageConfig(presetName);
   const presetIndex = path.join(
     exports.getDepPackagePath(presetName),
-    presetPkg.main || 'index.js'
-  )
+    presetPkg.main || "index.js"
+  );
 
-  return require(presetIndex)
-}
+  return require(presetIndex);
+};
 
 /**
  * Scans the project directory and collects configuration from all known sources:
@@ -168,13 +169,13 @@ exports.getProjectConfig = function (context) {
   // When processing the configuration tree, we consider 'parameters'
   // anything that does not start with a '_', that is reserved for the
   // preset configuration. For example 'webpack' or 'jest', etc.
-  const expandOnlyConfigBranches = (key) => key[0] !== '_'
+  const expandOnlyConfigBranches = (key) => key[0] !== "_";
 
   // Figure out the preset to use
-  const packageConfig = exports.getPackageConfig()
-  const userPkgConfig = packageConfig.elios || {}
-  const userConfig = exports.getUserConfig()
-  const presetName = exports.getPresetName(userConfig, packageConfig)
+  const packageConfig = exports.getPackageConfig();
+  const userPkgConfig = packageConfig.eilos || {};
+  const userConfig = exports.getUserConfig();
+  const presetName = exports.getPresetName(userConfig, packageConfig);
 
   // Progressively merge configuration from:
   // 1) Preset
@@ -182,35 +183,38 @@ exports.getProjectConfig = function (context) {
     exports.getPresetConfig(presetName),
     context,
     expandOnlyConfigBranches
-  )
-  context.updateConfig(presetConfig)
+  );
+  context.updateConfig(presetConfig);
 
   // 2) User config from `package.json``
-  context.updateConfig(userPkgConfig)
+  context.updateConfig(userPkgConfig);
 
   // 3) User config from `.eilos.js`
   const expandedUserConfig = expandParametricConfig(
     userConfig,
     context,
     expandOnlyConfigBranches
-  )
-  context.updateConfig(expandedUserConfig)
+  );
+  context.updateConfig(expandedUserConfig);
 
   // Collect actions configuration
-  const actions = merge.all([
-    presetConfig._actions || {},
-    userPkgConfig._actions || {},
-    expandedUserConfig._actions || {}
-  ], { arrayMerge: overwriteMerge })
+  const actions = merge.all(
+    [
+      presetConfig._actions || {},
+      userPkgConfig._actions || {},
+      expandedUserConfig._actions || {},
+    ],
+    { arrayMerge: overwriteMerge }
+  );
 
-  return new ProjectConfig(actions, context)
-}
+  return new ProjectConfig(actions, context);
+};
 
 exports.getDefaultProjectConfig = function () {
-  const context = defaultContextForProject(exports.getBasePath())
-  return exports.getProjectConfig(context)
-}
+  const context = defaultContextForProject(exports.getBasePath());
+  return exports.getProjectConfig(context);
+};
 
-exports.getConfigFile = function (component) {}
+exports.getConfigFile = function (component) {};
 
-exports.getConfigFilePath = function (component) {}
+exports.getConfigFilePath = function (component) {};

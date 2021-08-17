4b33a9c5d1f5261dab633ccbd7e77f960498d52d
Ioannis Charalampidis <johnys2@gmail.com>
2021-08-17 19:05:45 +0300

    fix: adding forgotten type exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { cli } from "./cli";
 export { invokeFileFunction } from "./files";
+
+// Types needed in preset
+export * from "./types/Action";
+export * from "./types/Preset";
+export * from "./struct/RuntimeContext";

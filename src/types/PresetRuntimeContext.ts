b21298bb56e52eddd7fc231cfe1513f37e5ae2f7
Ioannis Charalampidis <johnys2@gmail.com>
2021-08-19 21:08:58 +0300

    feat: adding helpers from runtime extraction

---
+++ src/types/PresetRuntimeContext.ts
@@ -0,0 +1,25 @@
+import type { RuntimeContext } from "../struct/RuntimeContext";
+import type { ConfigFiles } from "./ConfigFile";
+import type { PresetOption } from "./PresetOption";
+import type { PresetRuntimeConfig } from "./RuntimeConfig";
+
+type PresetOptionsFromConfig<C> = C extends { options: infer Opt }
+  ? Opt extends PresetOption
+    ? Opt
+    : {}
+  : {};
+
+type PresetFilesFromConfig<C> = C extends { files: infer Files }
+  ? Files extends ConfigFiles<any, any>
+    ? Files
+    : {}
+  : {};
+
+/**
+ * Extracts a runtime context for the given preset config
+ */
+export type PresetRuntimeContext<C> = RuntimeContext<
+  PresetRuntimeConfig<PresetOptionsFromConfig<C>>,
+  {},
+  PresetFilesFromConfig<C>
+>;
